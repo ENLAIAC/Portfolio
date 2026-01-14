e22c1bff237b8649c0efda19a564464ab4648b34
elia <eliacazzanti83@gmail.com>
2026-01-14 09:28:22 +0100

    Constants file

---
+++ fortran/HF/constants.f90
@@ -0,0 +1,8 @@
+program constants
+
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CONVERSIONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  double precision, parameter ev_to_Hartree = 1_dp/27.211407953_dp
+  double precision, parameter Hartree_to_ev = 27.211407953_dp
+
+end program
